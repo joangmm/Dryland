93b3dec421144ffbf09fd46f783c9430a4ce9e24
Angel Vicente <60506166+Angel-4-1@users.noreply.github.com>
2021-03-05 21:36:07 +0100

    move animal updated

--- assets/Scripts/move_animal.cs
+++ assets/Scripts/move_animal.cs
@@ -6,50 +6,66 @@ public class move_animal : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed;
-    private Vector3 rotate = new Vector3(0, 180, 0);
-    private bool backwards = false;
     private float nextActionTime = 0.0f;
     public float period = 0.1f;
     public Vector3 jump = new Vector3(0, 0, 0);
-    private Vector3 aux;
-    private bool obsInFront = false;
     private Camera raycast;
-    private RaycastHit hit;
+    private Vector3 initial;
 
     private Animation anim;
 
-    bool isMoving = false;
-
     void Start()
     {
+        raycast = GetComponent<Camera>();
+        initial = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        
-        
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.TransformDirection(Vector3.forward);
+        float maxDistance = 2;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast( origin, direction, out hit, maxDistance ) && hit.transform.tag == "Limit")
+        {
+            Debug.DrawRay( origin, direction * hit.distance, Color.yellow);
+            transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+
+            //change speed to go backwards
+            speed = speed * ( -1 ) ;
+        }
+        else
+        {
+            Debug.DrawRay(origin, direction * 100, Color.white);
+            //Debug.Log("Did Hit");
+        }
+
         if (Time.time > nextActionTime)
         {
             nextActionTime += period;
-            if (Physics.Raycast(transform.position, Vector3.forward, 2))
-            {
-                Debug.Log("LIMITEEE" + Vector3.forward.ToString());
-                Vector3 rot = transform.rotation.eulerAngles;
-                rot = new Vector3(rot.x, rot.y + 180, rot.z);
-                transform.rotation = Quaternion.Euler(rot);
-                
+            Vector3 target = transform.position;
 
-            }
-            
-            isMoving = true;
-            transform.position += Vector3.forward * speed + jump;
-            aux = new Vector3(Mathf.Floor(transform.position.x), Mathf.Floor(transform.position.y), Mathf.Floor(transform.position.z));
-            transform.position = aux;
-            
+            target += (Vector3.forward * speed + jump);
 
+            transform.position = new Vector3(Mathf.Floor(initial.x), Mathf.Floor(target.y), target.z );
         }
 
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        //Check for a match with the specific tag on any GameObject that collides with your GameObject
+        /*
+        if (collision.gameObject.tag == "Limit")
+        {
+            Vector3 rot = transform.rotation.eulerAngles;
+            rot = new Vector3(rot.x, rot.y + 180, rot.z);
+            transform.rotation = Quaternion.Euler(rot);
+            backwards ^= true;
+        }*/
     }
 }
